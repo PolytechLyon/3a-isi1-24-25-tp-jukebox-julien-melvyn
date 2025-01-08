d9378700b23cb622dc257bb7973614fbb64ae2a7
zulianc <julien.chataigner@etu.unic-lyon1.fr>
2025-01-08 15:42:54 +0100

    debut player

---
+++ Jukebox/src/components/Player.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { usePlayer } from "../composables/usePlayer.js";
+    const { isPlaying } = usePlayer();
+</script>
+
+<template>
+    <h2>Player</h2>
+    <div v-if="isPlaying">
+
+    </div>
+    <div v-else>
+        <p>Choose a track to play.</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Jukebox/src/composables/usePlayer.js
@@ -0,0 +1,7 @@
+const isPlaying = false;
+
+export function usePlayer() {
+    return {
+        isPlaying,
+    }
+}
